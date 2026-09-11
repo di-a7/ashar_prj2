fb2e365964be5ffc9429a2ec5d9d0795ab92d927
diya <shresthadiya59@gmail.com>
2026-09-11 07:56:46 +0545

    concrete view,viewset,modelviewset

--- rms/urls.py
+++ rms/urls.py
@@ -1,16 +1,20 @@
 from django.urls import path
 # from .views import category, categorydetail
-from .views import CategoryGenericAPIView, CategoryDetailAPIView
+from .views import CategoryModelViewset
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register(r"category", CategoryModelViewset, basename="snippet")
+
 urlpatterns = [
-   # Genric api
-   path('category/', CategoryGenericAPIView.as_view()),
-   path('category/<pk>/', CategoryDetailAPIView.as_view())
+   # Viewset api
+   # path('category/', CategoryViewset.as_view({'get':'list','post':'create'})),
+   # path('category/<pk>/', CategoryDetailViewset.as_view({'get':'retrieve','delete':'destroy','put':'update','patch':'partial_update'}))
    
-   # APIView
+   # APIView/GernericAPIView
    # path('category/', CategoryAPIView.as_view()),
    # path('category/<id>/', CategoryDetailAPIView.as_view()),
    
    # function api
    # path('category/', category)
    # path('category/<pk>/', categorydetail)
-]
+] + router.urls
--- rms/views.py
+++ rms/views.py
@@ -7,38 +7,114 @@ from rest_framework import status
 # Create your views here.
 
 # Class Based API
-# GenericAPI and Mixin
-from rest_framework.generics import GenericAPIView, mixins
-class CategoryGenericAPIView(GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+# ModelViewset
+from rest_framework import viewsets
+class CategoryModelViewset(viewsets.ModelViewSet):
    queryset = Category.objects.all()
    serializer_class = CategorySerializer
    
-   def get(self,request):
-      return self.list(request)
-   
-   def post(self,reqeust):
-      return self.create(self.request)
-
-class CategoryDetailAPIView(GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
-   queryset = Category.objects.all()
-   serializer_class = CategorySerializer
-   
-   def get(self,request, pk):
-      return self.retrieve(request, pk)
-   
-   def put(self, request, pk):
-      return self.update(request, pk)
-   
-   def patch(self, request, pk):
-      return self.partial_update(request, pk)
-      
-   def delete(self, request, pk):
+   def destroy(self,request,pk):
       category = Category.objects.get(pk=pk)
       item = OrderItem.objects.filter(food__category = category).count()
       if item > 0 :
          return Response({"message":"Can not be deleted. Category related to Food in OrderItem."})
       category.delete()
       return Response({"message":"Data deleted."}, status=status.HTTP_204_NO_CONTENT)
+
+
+# Viewset
+# class CategoryViewset(viewsets.ViewSet):
+#    def list(self,request):
+#       category = Category.objects.all()
+#       serializer = CategorySerializer(category, many=True)
+#       return Response(serializer.data)
+   
+#    def create(self,request):
+#       serializer = CategorySerializer(data=request.data)   # deserialization: convert json data into python data
+#       serializer.is_valid(raise_exception = True)
+#       serializer.save()
+#       return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+#    def retrieve(self,request,pk):
+#       category = Category.objects.get(pk=pk)
+#       serializer = CategorySerializer(category)
+#       return Response(serializer.data)
+   
+#    def destroy(self,request,pk):
+#       category = Category.objects.get(pk=pk)
+#       item = OrderItem.objects.filter(food__category = category).count()
+#       if item > 0 :
+#          return Response({"message":"Can not be deleted. Category related to Food in OrderItem."})
+#       category.delete()
+#       return Response({"message":"Data deleted."}, status=status.HTTP_204_NO_CONTENT)
+   
+#    def update(self,request,pk):
+#       category = Category.objects.get(pk=pk)
+#       serializer = CategorySerializer(category, data=request.data)
+#       serializer.is_valid(raise_exception = True)
+#       serializer.save()
+#       return Response(serializer.data)
+   
+#    def partial_update(self,request,pk):
+#       category = Category.objects.get(pk=pk)
+#       serializer = CategorySerializer(category, data=request.data, partial=True)
+#       serializer.is_valid(raise_exception = True)
+#       serializer.save()
+#       return Response(serializer.data)
+
+
+# Concrete View Classes
+# from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+# class CategoryGenericAPIView(ListCreateAPIView):
+#    queryset = Category.objects.all()
+#    serializer_class = CategorySerializer
+
+# class CategoryDetailAPIView(RetrieveUpdateDestroyAPIView):
+#    queryset = Category.objects.all()
+#    serializer_class = CategorySerializer
+   
+#    def delete(self, request, pk):
+#       category = Category.objects.get(pk=pk)
+#       item = OrderItem.objects.filter(food__category = category).count()
+#       if item > 0 :
+#          return Response({"message":"Can not be deleted. Category related to Food in OrderItem."})
+#       category.delete()
+#       return Response({"message":"Data deleted."}, status=status.HTTP_204_NO_CONTENT)
+
+
+
+# GenericAPI and Mixin
+# from rest_framework.generics import GenericAPIView, mixins
+# class CategoryGenericAPIView(GenericAPIView, mixins.ListModelMixin):
+#    queryset = Category.objects.all()
+#    serializer_class = CategorySerializer
+   
+#    def get(self,request):
+#       return self.list(request)
+   
+   # def post(self,reqeust):
+   #    return self.create(self.request)
+
+# class CategoryDetailAPIView(GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+#    queryset = Category.objects.all()
+#    serializer_class = CategorySerializer
+   
+#    def get(self,request, pk):
+#       return self.retrieve(request, pk)
+   
+#    def put(self, request, pk):
+#       return self.update(request, pk)
+   
+#    def patch(self, request, pk):
+#       return self.partial_update(request, pk)
+      
+#    def delete(self, request, pk):
+#       category = Category.objects.get(pk=pk)
+#       item = OrderItem.objects.filter(food__category = category).count()
+#       if item > 0 :
+#          return Response({"message":"Can not be deleted. Category related to Food in OrderItem."})
+#       category.delete()
+#       return Response({"message":"Data deleted."}, status=status.HTTP_204_NO_CONTENT)
 
 # GenericAPIView
 # class CategoryGenericAPIView(GenericAPIView):
@@ -70,64 +146,64 @@ class CategoryDetailAPIView(GenericAPIView, mixins.RetrieveModelMixin, mixins.Up
 
 
 # APIView
-from rest_framework.views import APIView
+# from rest_framework.views import APIView
 
-class CategoryAPIView(APIView):
-   def get(self,request):
-      category = Category.objects.all()
-      serializer = CategorySerializer(category, many=True)
-      return Response(serializer.data)
+# class CategoryAPIView(APIView):
+#    def get(self,request):
+#       category = Category.objects.all()
+#       serializer = CategorySerializer(category, many=True)
+#       return Response(serializer.data)
    
-   def post(self,request):
-      serializer = CategorySerializer(data=request.data)   # deserialization: convert json data into python data
-      serializer.is_valid(raise_exception = True)
-      serializer.save()
-      return Response(serializer.data, status=status.HTTP_201_CREATED)
+#    def post(self,request):
+#       serializer = CategorySerializer(data=request.data)   # deserialization: convert json data into python data
+#       serializer.is_valid(raise_exception = True)
+#       serializer.save()
+#       return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-class CategoryDetailAPIView(APIView):
-   def get(self,request,pk):
-      category = Category.objects.get(pk=pk)
-      serializer = CategorySerializer(category)
-      return Response(serializer.data)
+# class CategoryDetailAPIView(APIView):
+#    def get(self,request,pk):
+#       category = Category.objects.get(pk=pk)
+#       serializer = CategorySerializer(category)
+#       return Response(serializer.data)
    
-   def delete(self,request,pk):
-      category = Category.objects.get(pk=pk)
-      item = OrderItem.objects.filter(food__category = category).count()
-      if item > 0 :
-         return Response({"message":"Can not be deleted. Category related to Food in OrderItem."})
-      category.delete()
-      return Response({"message":"Data deleted."}, status=status.HTTP_204_NO_CONTENT)
+#    def delete(self,request,pk):
+#       category = Category.objects.get(pk=pk)
+#       item = OrderItem.objects.filter(food__category = category).count()
+#       if item > 0 :
+#          return Response({"message":"Can not be deleted. Category related to Food in OrderItem."})
+#       category.delete()
+#       return Response({"message":"Data deleted."}, status=status.HTTP_204_NO_CONTENT)
    
-   def put(self,request,pk):
-      category = Category.objects.get(pk=pk)
-      serializer = CategorySerializer(category, data=request.data)
-      serializer.is_valid(raise_exception = True)
-      serializer.save()
-      return Response(serializer.data)
+#    def put(self,request,pk):
+#       category = Category.objects.get(pk=pk)
+#       serializer = CategorySerializer(category, data=request.data)
+#       serializer.is_valid(raise_exception = True)
+#       serializer.save()
+#       return Response(serializer.data)
    
-   def patch(self,request,pk):
-      category = Category.objects.get(pk=pk)
-      serializer = CategorySerializer(category, data=request.data, partial=True)
-      serializer.is_valid(raise_exception = True)
-      serializer.save()
-      return Response(serializer.data)
+#    def patch(self,request,pk):
+#       category = Category.objects.get(pk=pk)
+#       serializer = CategorySerializer(category, data=request.data, partial=True)
+#       serializer.is_valid(raise_exception = True)
+#       serializer.save()
+#       return Response(serializer.data)
 
 
 
 
 # Functioin Based API
-@api_view(['GET','POST'])
-def category(request):
-   if request.method == 'GET':
-      category = Category.objects.all()
-      serializer = CategorySerializer(category, many=True) # serializer: convert queryset into json format
-      return Response(serializer.data)
+# @api_view(['GET','POST'])
+# def category(request):
+#    if request.method == 'GET':
+#       category = Category.objects.all()
+#       serializer = CategorySerializer(category, many=True) # serializer: convert queryset into json format
+#       return Response(serializer.data)
 
-   elif request.method == 'POST':
-      serializer = CategorySerializer(data=request.data)   # deserialization: convert json data into python data
-      serializer.is_valid(raise_exception = True)
-      serializer.save()
-      return Response(serializer.data, status=status.HTTP_201_CREATED)
+#    elif request.method == 'POST':
+#       serializer = CategorySerializer(data=request.data)   # deserialization: convert json data into python data
+#       serializer.is_valid(raise_exception = True)
+#       serializer.save()
+#       return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 # @api_view(['GET','PUT','DELETE'])
 # def categorydetail(request, pk):
